4eb6d4eb07542cf252ffa30ab05feb7dbdeb48bd
Madhava23 <168384844+Madhava23@users.noreply.github.com>
2026-07-01 18:50:17 +0530

    Update SESSION_ID replacement string in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const { connectDB, getBotSettings, updateSetting } = require("./plugins/bot_db")
 
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("MALVIN-XD~", '');
+const sessdata = config.SESSION_ID.replace("𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
